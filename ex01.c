965d35b7360ae0944ff6422c435ac55ee8832f2a
JOHNNY111111111111 <156683286+JOHNNY111111111111@users.noreply.github.com>
2024-01-15 15:20:56 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int e = 0;
+    float m  = 3.1;
+    char i = 'r';
+    printf("%d\n", e);
+    printf("%f\n", m);
+    printf("%c\n", i);
+
+    return 0;
+}
