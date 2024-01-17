f2319b2909705502afd1f2e24bfba4e4328c97a5
JOHNNY111111111111 <156683286+JOHNNY111111111111@users.noreply.github.com>
2024-01-17 09:52:10 +0100

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main(){
+
+
+    int johnny;
+    int depp;
+
+    printf("quanto ha preso johnny alla verifica?:\n");
+    scanf("%d", &johnny);
+    printf("e depp?");
+    scanf("%d", &depp);
+    if(johnny > depp){
+        printf("bravo johnny sei più intelligente di depp\n");
+    }else if(johnny < depp){
+        printf("johnny fa schifo e depp è un genio\n");
+    }else if(johnny == depp){
+
+        printf("avete copiato\n");
+        if(avete copiato){
+        if(johnny > 6); 
+        printf("avete copiato bene\n");
+         if(depp < 6);
+        printf("avete copiato e pure male stupidi\n");
+        }
+        
+    }
+    
+    return 0;
+}
