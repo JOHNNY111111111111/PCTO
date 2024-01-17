c9ccd85a695eb9e042b8498e9d8a134b5ed5fb79
JOHNNY111111111111 <156683286+JOHNNY111111111111@users.noreply.github.com>
2024-01-17 16:08:46 +0100

    Add files via upload

---
+++ ex16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    
+    int primo;
+    int x = 2;
+    printf("dimme un numero\n");
+    scanf("%d", &primo);
+    
+        if( i == 1){
+        printf("si\n");
+
+    }
+    else{
+        while(n%x!=0){
+            x = x+1;
+        }
+    }if(n==x);
+        printf("si\n)");
+    }else{
+        printf("no")
+    }
+    
+    return 0;
+}
